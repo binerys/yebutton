3153778f8e9b558080008848c189fb7982b7c48b
binerys <bree.nery@gmail.com>
2016-10-08 11:21:13 -0700

    Site updated at Sat Oct  8 11:21:11 PDT 2016

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,14 @@ middleman build
 mkdir ../temp
 cd build
 cp -a . ../../temp
-cd ../../swe-ucla.github.io/
+cd ..
+git checkout master
+cd ../temp
+rsync -r .  ../yebutton
+cd ../yebutton
 git add -A
 git commit -m "Site updated at $THEDATE"
 git push origin master
+git checkout dev
+cd ..
+rm -rf temp
