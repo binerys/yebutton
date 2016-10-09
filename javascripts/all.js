bb9e1e1322855586dc519ba9f30891a0bfad312d
binerys <bree.nery@gmail.com>
2016-10-08 21:22:59 -0700

    Site updated at Sat Oct  8 21:22:56 PDT 2016

--- javascripts/all.js
+++ javascripts/all.js
@@ -1,1 +1,15 @@
 // This is where it all goes :)
+function play()
+{
+    var audio = document.getElementById("music");
+    if (audio.paused)
+    {
+        audio.play();
+    } else {
+        audio.pause();
+        audio.currentTime = 0; 
+        audio.play();
+    }
+
+	
+}
